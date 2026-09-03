05a41ff1e052c89a460dc54b0eab448cd24888db
HyggshiOSDeveloper <hyggshidev@gmail.com>
2026-09-03 18:39:07 +0700

    fix code fork icon

--- lib/renderer/resource-icons.ts
+++ lib/renderer/resource-icons.ts
@@ -82,7 +82,7 @@ export const RESOURCE_ICONS: Record<string, IconData> = {
   },
   gitfork: {
     viewBox: "0 0 24 24",
-    defaultColor: "#000000",
+    defaultColor: "#ffffffff",
     svgContent: "<g fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\"><path d=\"M10 18a2 2 0 1 0 4 0a2 2 0 1 0-4 0M5 6a2 2 0 1 0 4 0a2 2 0 1 0-4 0m10 0a2 2 0 1 0 4 0a2 2 0 1 0-4 0\"/><path d=\"M7 8v2a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V8m-5 4v4\"/></g>"
   },
   gitlab: {
